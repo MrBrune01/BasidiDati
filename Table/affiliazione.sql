90bf443e5b8d15f37e28f91d7bd5efc08ba549a7
Mattia <mattia.brunello@studenti.unipd.it>
2022-05-25 23:46:25 +0200

    Aggiunti altri dati

--- Table/affiliazione.sql
+++ Table/affiliazione.sql
@@ -5,7 +5,7 @@ CREATE TABLE affiliazione (
 	id_account VARCHAR(25) NOT NULL,
 	tipo_affiliazione VARCHAR(25) NOT NULL,
 	metodo_di_pagamento VARCHAR(25) NOT NULL,
-	guadagni_totali INT,
+	guadagni_totali MONEY NOT NULL,
 	PRIMARY KEY (id_affiliazione),
 	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE
 );
--- Table/categoria.sql
+++ Table/categoria.sql
@@ -4,3 +4,18 @@ CREATE TABLE categoria (
 	nome VARCHAR(25) NOT NULL,
 	PRIMARY KEY (id_clip)
 );
+
+insert into categoria (id_categoria, nome) values
+(1, 'HotTub'),
+(2, 'Fortinte'),
+(3, 'Fifa'),
+(4, 'AssettoCorsa'),
+(5, 'Minecraft'),
+(6, 'Factorio'),
+(7, 'Sport'),
+(8, 'Elden Ring'),
+(9, 'Apex Legend'),
+(10, 'CounterStrike'),
+(11, 'Valorant'),
+(12, 'GtaV'),
+(13, 'Dota2');
