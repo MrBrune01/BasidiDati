7a6a80c5ed880d1cad3590b3c59466082b5730c2
Mattia <mattia.brunello@studenti.unipd.it>
2022-05-27 19:38:12 +0200

    Finito in teoria

---
+++ Table/main.sql
@@ -0,0 +1,139 @@
+DROP TABLE IF EXISTS account;
+DROP TYPE IF EXISTS tipo_account;
+DROP TABLE IF EXISTS canale;
+DROP TABLE IF EXISTS abbonati;
+DROP TABLE IF EXISTS azienda;
+DROP TABLE IF EXISTS affiliazione;
+DROP TABLE IF EXISTS clip;
+DROP TABLE IF EXISTS diretta;
+DROP TABLE IF EXISTS visiona;
+DROP TABLE IF EXISTS emote;
+DROP TABLE IF EXISTS modera;
+DROP TABLE IF EXISTS seguiti;
+DROP TABLE IF EXISTS sponsorizza;
+CREATE TYPE tipo_account AS ENUM ('streamer', 'spettatore', 'moderatore');
+CREATE TABLE account (
+	username VARCHAR(25) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	user_pass VARCHAR(64) NOT NULL,
+	user_type tipo_account NOT NULL,
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE affiliazione (
+	id_affiliazione INT NOT NULL,
+	id_account VARCHAR(25) NOT NULL,
+	tipo_affiliazione VARCHAR(25) NOT NULL,
+	metodo_di_pagamento VARCHAR(25) NOT NULL,
+	guadagni_totali MONEY NOT NULL,
+	PRIMARY KEY (id_affiliazione),
+	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE canale (
+	nome VARCHAR(15) NOT NULL,
+	informazioni VARCHAR(100) NOT NULL,
+	id_streamer VARCHAR(250) NOT NULL,
+	PRIMARY KEY (nome),
+	FOREIGN KEY (id_streamer) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE azienda (
+	p_iva VARCHAR(11) NOT NULL,
+	email_azienda VARCHAR(25) NOT NULL,
+	nome_azienda VARCHAR(25) NOT NULL,
+	PRIMARY KEY (p_iva)
+);
+
+CREATE TABLE sponsorizza (
+	p_iva VARCHAR(15) NOT NULL,
+	id_canale VARCHAR(25) NOT NULL,
+	pagamento INT NOT NULL,
+	PRIMARY KEY (p_iva,id_canale),
+	FOREIGN KEY (p_iva) REFERENCES azienda(p_iva) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS categoria;
+CREATE TABLE categoria (
+	id_categoria INT NOT NULL,
+	nome VARCHAR(25) NOT NULL,
+	PRIMARY KEY (id_clip)
+);
+
+CREATE TABLE diretta (
+	id_diretta INT NOT NULL,
+	id_canale VARCHAR(25) NOT NULL,
+	titolo VARCHAR(25) NOT NULL,
+	ora_inizio  TIMESTAMP NOT NULL,
+	visualizzazioni INT NOT NULL,
+	categoria INT NOT NULL
+	PRIMARY KEY (id_diretta),
+	FOREIGN KEY (id_canale) REFERENCES canale(nome_canale) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (categoria) REFERENCES categoria(id_categoria) ON UPDATE CASCADE ON DELETE CASCADE
+);
+CREATE TABLE clip (
+	id_clip INT NOT NULL,
+	id_canale VARCHAR(52) NOT NULL,
+	titolo VARCHAR(52) NOT NULL,
+	durata INT NOT NULL,
+	file_loc  VARCHAR(100) NOT NULL,
+	id_account VARCHAR(52) NOT NULL,
+	PRIMARY KEY (id_clip),
+	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE abbonati (
+	id_account VARCHAR(25) NOT NULL,
+	id_canale VARCHAR(25) NOT NULL,
+	costoAbbonamento DECIMAL NOT NULL,
+	PRIMARY KEY (id_account,id_canale),
+	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE visiona (
+	id_account VARCHAR(25) NOT NULL,
+	id_diretta INT NOT NULL,
+	PRIMARY KEY (id_account,id_diretta),
+	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_diretta) REFERENCES diretta(id_diretta) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE emote (
+	nome VARCHAR(25) NOT NULL,
+	livello INT NOT NULL,
+	link VARCHAR(25) NOT NULL,
+	id_canale VARCHAR(15) NOT NULL,
+	PRIMARY KEY (nome),
+	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE modera (
+	id_account VARCHAR(15) NOT NULL,
+	id_canale VARCHAR(25) NOT NULL,
+	livello VARCHAR(12) NOT NULL,
+	PRIMARY KEY (id_account,id_canale)
+	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE seguiti (
+	id_utente VARCHAR(25) NOT NULL,
+	id_canale VARCHAR(25) NOT NULL,
+	PRIMARY KEY (id_utente,id_canale),
+	FOREIGN KEY (id_utente) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS commenta;
+CREATE TABLE commenta (
+	id_account VARCHAR(25) NOT NULL,
+	id_diretta VARCHAR(25) NOT NULL,
+	commento VARCHAR(50) NOT NULL,
+	PRIMARY KEY (id_account,id_diretta),
+	FOREIGN KEY (id_account) REFERENCES account(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_diretta) REFERENCES diretta(id_diretta) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
--- Table/moderatori.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS moderatori;
-CREATE TABLE moderatori (
-	id_utente VARCHAR(25) NOT NULL,
-	id_canale VARCHAR(25) NOT NULL,
-	PRIMARY KEY (id_utente,id_canale),
-	FOREIGN KEY (id_utente) REFERENCES utente(username) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (id_canale) REFERENCES canale(nome) ON UPDATE CASCADE ON DELETE CASCADE
-);
