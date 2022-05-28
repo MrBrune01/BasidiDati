15d026df44b00d322dc9addacf5436285588bb8c
Mattia <mattia.brunello@studenti.unipd.it>
2022-05-28 13:00:24 +0200

    Seconda query

--- query.sql
+++ query.sql
@@ -1,8 +1,32 @@
 --Numero totale di visualizzazione di una categoria
+SELECT COUNT(account.username), categoria.nome
+FROM diretta 
+JOIN categoria on diretta.categoria=categoria.id_categoria
+JOIN guarda on guarda.id_diretta=diretta.id_diretta
+JOIN account on account.username=guarda.id_account
+GROUP BY categoria.nome;
 
-SELECT nome
-FROM categoria,
 
-SELECT id_diretta
-FROM guarda as G1, guarda as G2
-WHERE G1.id_diretta=G2.id_diretta AND G1.id_account!=G2.id_account;
+-- Canale con il guadagno più alto sul sito
+SELECT MAX(SELECT SUM(costoAbbonamento),canale.nome
+FROM abbonati
+JOIN canale on canale.nome=abbonati.id_canale
+GROUP BY canale.nome);
+
+
+
+SELECT SUM(costoAbbonamento) as total_amount
+FROM abbonati
+JOIN canale on canale.nome=abbonati.id_canale
+GROUP BY canale.nome
+ 
+
+
+
+SELECT canale.nome, somme.totale FROM
+    WHERE somme.totale = (MAX(somme.totale) FROM(
+    SELECT SUM(costoAbbonamento) as totale, canale.nome
+	FROM abbonati
+	JOIN canale on canale.nome=abbonati.id_canale
+	GROUP BY canale.nome 
+    )) as somme
